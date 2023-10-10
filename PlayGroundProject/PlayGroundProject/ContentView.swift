b9720750e2d2458275485e1d9548801dbed98c6b
Henny2 <henrieke.baunack@web.de>
2023-10-09 20:25:30 -0700

    adding project to work on basics

---
+++ PlayGroundProject/PlayGroundProject/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PlayGroundProject
+//
+//  Created by Henrieke Baunack on 10/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form{
+            Text("Hi")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayGroundProject/PlayGroundProject/PlayGroundProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayGroundProjectApp.swift
+//  PlayGroundProject
+//
+//  Created by Henrieke Baunack on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayGroundProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
