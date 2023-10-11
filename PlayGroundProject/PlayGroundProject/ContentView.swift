b26b194a480834fcdd7c23fafcac9caf5692d532
Henny2 <henrieke.baunack@web.de>
2023-10-10 19:56:56 -0700

    playing around with basics in swiftUI

--- PlayGroundProject/PlayGroundProject/ContentView.swift
+++ PlayGroundProject/PlayGroundProject/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username = ""
+    @State var selectedStudent = "Henny"
+    let students = ["Henny", "John", "Mary"]
     var body: some View {
         NavigationStack{
             Form{
-                Text("Hi")
+                Section{
+                    TextField("Put in your name please", text: $username)
+                    Text("Hi \(username)")
+                }
+                Section{
+                    Picker("Select a student", selection: $selectedStudent){
+                        ForEach(students, id: \.self){
+                            Text($0)
+                        }
+                    }
+                }
+                
             }
-            
+            Form{
+                ForEach(0..<10){ num in
+                    Text("Row 2")
+                }
+            }
             .navigationTitle("Henny's First App")
         }
         
